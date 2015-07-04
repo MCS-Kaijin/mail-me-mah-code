900b4ca6817d2ace21df174071bae2e3e54d8c2d
MCS-Kaijin <MCS-Kaijin@users.noreply.github.com>
2015-07-04 17:19:32 -0500

    Update main.py
    Code now actually does something. It emails the contents of your files to the recipient, but not the files themselves.

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 # coding: utf-8
 
-print "Hello, World!"
+import os
+import re
+
+from smtplib import SMTP
+
+sender = input("Your email: ")
+sender = re.match((r'(?P<usr>[\w]+)@(?P<host>.+)'), sender)
+password = input("Password: ")
+recipient = input("To: ")
+
+folder = input("Folder containing project: ")
+mstr = ''
+for filename in os.listdir(folder):
+        fl = open(folder+'/'+filename, 'r')
+        mstr = mstr+fl.read()+'\n\n----\n\n'
+
+msg = "From: {}@{}\r\nTo: {}\r\nSubject: {}\r\n\r\n{}".format(sender.group("usr"), sender.group("host"), recipient, folder, mstr)
+
+smtp = SMTP("smtp.gmail.com")
+smtp.starttls()
+smtp.login(sender.group("usr"),password)
+smtp.sendmail(sender.group("usr")+"@"+sender.group("host"),recipient,msg)
+smtp.quit()
