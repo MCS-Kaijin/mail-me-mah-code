8cb1e56e68845764586139b84ccf9770f0b08f04
MCS-Kaijin <MCS-Kaijin@users.noreply.github.com>
2015-07-04 21:32:03 -0500

    Update main.py
    The code now does what the readme says it does. Except for maybe some aesthetic updates, this should be the last commit.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-# coding: utf-8
-
 import os
 import re
+import zipfile
+import tempfile
 
 from smtplib import SMTP
 
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from email.MIMEBase import MIMEBase
 from email import encoders
+from email.message import Message
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 sender = raw_input("Your email: ")
 sender = re.match((r'(?P<usr>[\w]+)@(?P<host>.+)'), sender)
@@ -16,18 +17,28 @@ password = raw_input("Password: ")
 recipient = raw_input("To: ")
 
 folder = raw_input("Folder containing project: ")
+attach = zipfile.ZipFile('../code.zip', 'w')
 mstr = ''
-for filename in os.listdir(folder):
-        fl = open(folder+'/'+filename, 'r')
-        mstr = mstr+fl.read()+'\n\n----\n\n'
+for root, dirs, files in os.walk(folder):
+	for fl in files:
+		attach.write(os.path.join(root, fl))
+attach = open("../code.zip","r")
 
 msg = MIMEMultipart()
 msg['From'] = sender.group("usr")+"@"+sender.group("host")
 msg['To'] = recipient
 msg['Subject'] = folder
+msg.attach(MIMEText(mstr,'plain'))
 
-smtp = SMTP("smtp.gmail.com")
+part = MIMEBase('application', 'zip')
+part.set_payload(attach.read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="../code.zip"')
+
+msg.attach(part)
+
+smtp = SMTP("smtp."+sender.group("host"))
 smtp.starttls()
 smtp.login(sender.group("usr"),password)
-smtp.sendmail(sender.group("usr")+"@"+sender.group("host"),recipient,msg)
+smtp.sendmail(sender.group("usr")+"@"+sender.group("host"),recipient,msg.as_string())
 smtp.quit()
